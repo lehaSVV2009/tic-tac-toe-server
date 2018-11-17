0fb4572161b76cd10c963e6d982bc659adf37f30
Alex Soroka <lehaSVV2009@gmail.com>
2018-11-17 12:58:10 +0300

    Add port env variable for heroku

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import { Server } from "boardgame.io/server";
 import { Game } from "tic-tac-toe-core";
 
+const port = process.env.PORT || 8000;
 const server = Server({ games: [Game] });
-server.run(8000);
+
+server.run(port, () => {
+  console.log("App is running on port " + port);
+});
